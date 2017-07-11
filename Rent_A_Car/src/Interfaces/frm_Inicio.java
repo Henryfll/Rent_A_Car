97d96107bc64f8df4cc5968e9c68acc515d0d7ee
Jonathan <jonita_ryu@hotmail.com>
2017-07-10 23:26:00 -0500

    Registro de Usuarios
    Se registro al usuario.

--- Rent_A_Car/src/Interfaces/frm_Inicio.java
+++ Rent_A_Car/src/Interfaces/frm_Inicio.java
@@ -36,6 +36,7 @@ public class frm_Inicio extends javax.swing.JFrame {
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -77,6 +78,14 @@ public class frm_Inicio extends javax.swing.JFrame {
             }
         });
         jMenu1.add(jMenuItem3);
+
+        jMenuItem4.setText("Usuarios");
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem4);
 
         jMenuBar1.add(jMenu1);
 
@@ -121,6 +130,13 @@ public class frm_Inicio extends javax.swing.JFrame {
         g_cli.show();
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+        // TODO add your handling code here:
+        Usuarios usuarios = new Usuarios();
+        panelPrincipal.add(usuarios);
+        usuarios.show();
+    }//GEN-LAST:event_jMenuItem4ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -163,6 +179,7 @@ public class frm_Inicio extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
     public static javax.swing.JDesktopPane panelPrincipal;
     // End of variables declaration//GEN-END:variables
 }
